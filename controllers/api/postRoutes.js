9989565734fad1fcb34e15904f00047a84087aaf
Jason897571 <king.gs1314@outlook.com>
2024-03-04 15:26:32 -0500

    complete edit and delete api for post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -22,4 +22,39 @@ router.post('/new', async(req,res)=>{
 
 })
 
+
+router.put('edit',async(req,res)=>{
+    try{
+        const title = req.body.title;
+        const content = req.body.content;
+    
+        await Post.create({
+            title: title,
+            contents:content,
+            user_id:req.session.user_id
+    
+        })
+        res.status(200).json({"message":"post created","title":title,"content":content});
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+    
+
+})
+
+
+router.delete('/delete',async(req,res)=>{
+    try{
+        await Post.destroy({
+            where:{
+                post_id:req.body.id
+            }
+        })
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
