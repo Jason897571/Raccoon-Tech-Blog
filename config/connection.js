2792a89bf94f6ccf54e25ad6e6bdb5aafd2734c3
Jason897571 <king.gs1314@outlook.com>
2024-03-06 08:02:11 -0500

    to set up herku database

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 
 if(process.env.JAWSDB_URL){
-  const sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 }else{
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {User} = require('../../models');
-const bcrypt = require('bcrypt');
+
 
 
 router.post('/login', async (req, res) => {
